d81a1cb2a192964a074666d5822be8a5f18c6ab5
vguenoff <vguenoff@gmail.com>
2020-05-31 17:40:58 +0300

    Remove App

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import 'normalize.css'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import GlobalStyle from './GlobalStyle'
+import configureStore from './store'
+import Header from './components/Header'
+import Main from './Main'
+
+const { store, persistor } = configureStore()
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider {...{ store }}>
+            <PersistGate {...{ persistor }} loading={null}>
+                <GlobalStyle />
+                <Router>
+                    <Header />
+                    <Main />
+                </Router>
+            </PersistGate>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 )
